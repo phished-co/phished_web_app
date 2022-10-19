e5cb2b0d8908bdc0e3a684bf8bd744a095e6c431
SueLee <slee1008@my.bcit.ca>
2022-10-18 20:15:16 -0700

    create button comp

--- components/button/Button.jsx
+++ components/button/Button.jsx
@@ -1,10 +1,30 @@
-import styles from './Button.module.css';
+import styled from 'styled-components';
 
-export default function Button(props) {
-  return <div className={styles.container}>
-    <button className={`
-      ${styles.button}
-      ${props.primary ? styles.primary : styles.secondary}
-    `}>{props.label}</button>
-  </div>
+
+const PrimaryBtn = styled.button`
+display: flex;
+align-items: flex-start;
+padding: 10px 20px;
+border-radius: 4px;
+background-color: ${props=>props.bg};
+text-decoration: none;
+font-family: 'Lexend', Tahoma, Geneva, Verdana, sans-serif;
+color: ${props=>props.cl};
+font-size: ${props=>props.sz};
+`
+
+
+
+export default function Button({
+  size = '12px',
+  bgcolor = '#171717',
+  txtcolor = '#FFFFFF',
+  txt = 'Primary',
+  type = 'type'
+}) {
+  return (
+  <PrimaryBtn sz={size} bg={bgcolor} cl={txtcolor} type = {type}>{txt}</PrimaryBtn>
+  )
+  
+  
 }
