305490664da224f2668d789f35cf7099788741f7
Trevor Lee <tlee297@my.bcit.ca>
2022-10-19 22:06:45 -0700

    add fb-func and fb-admin and henry fix storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
-// import "../styles/globals.css";
+import "../styles/globals.css";
+import '@tremor/react/dist/esm/tremor.css'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,5 +8,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  previewTabs: {
+    'storybook/docs/panel': { index: -1 },
   },
 }
