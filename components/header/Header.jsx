13592bcc193bffbf3f0aa2b301b88bc0348c5245
Lucia-martin <90855217+Lucia-martin@users.noreply.github.com>
2022-10-19 21:43:52 -0700

    firebase config + functions set up - not connected to app yet - nodemailer function separated into file - added emailScheduled api route - connected schedule button in phish form to the api route (passes info)

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -6,132 +6,114 @@ import Image from 'next/image';
 import { Bars3Icon } from '@heroicons/react/20/solid';
 import styled from 'styled-components';
 
-
 const NavCont = styled.nav`
-display: flex;
-justify-content: space-between;
-align-items: flex-end
-flex-direction: column;
-background-color: #2830f8;
-width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end
+  flex-direction: column;
+  background-color: #2830f8;
+  width: 100vw;
 
-@media only screen and (max-width: 479px){
-  & {
-    height: 7vh;
-    align-items: center;
-  }
-}
-
-@media only screen and (min-width: 480px) {
-   
-  & {
-    height: 72px;
+  @media only screen and (max-width: 479px) {
+    & {
+      height: 7vh;
+      align-items: center;
+    }
   }
 
-} 
-
-`
+  @media only screen and (min-width: 480px) {
+    & {
+      height: 72px;
+    }
+  }
+`;
 
 const MenuCont = styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: space-between;
-margin: 1.5vw 3vw;
-`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 1.5vw 3vw;
+`;
 
 const Menu = styled.a`
-margin: 0 1vw;
-color: white;
-text-decoration: none;
-font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-cursor: pointer;
+  margin: 0 1vw;
+  color: white;
+  text-decoration: none;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  cursor: pointer;
 
-@media only screen and (max-width: 480px) {
-   
-  & {
-    display:none;
+  @media only screen and (max-width: 480px) {
+    & {
+      display: none;
+    }
   }
 
-} 
-
-@media only screen and (min-width: 481px, max-width: 768px) {
-   
-  & {
-    display:none;
+  @media only screen and (min-width: 481px, max-width: 768px) {
+    & {
+      display: none;
+    }
   }
-
-}
-`
+`;
 
 const MyImg = styled(Image)`
-min-width: 100px;
-min-height: 33px;
-margin-right: 1vw;
-cursor: pointer;
-` 
+  min-width: 100px;
+  min-height: 33px;
+  margin-right: 1vw;
+  cursor: pointer;
+`;
 
 const Hamburger = styled.button`
-display: none;
+  display: none;
 
-@media only screen and (max-width: 480px) {
-
-  & {
-    display: block;
-    color: #fff;
-    width: 30px;
-    height: 30px;
-    align-items: center;
-    justify-content: center;
-    margin: 1vw;
-    cursor: pointer;
-
+  @media only screen and (max-width: 480px) {
+    & {
+      display: block;
+      color: #fff;
+      width: 30px;
+      height: 30px;
+      align-items: center;
+      justify-content: center;
+      margin: 1vw;
+      cursor: pointer;
+    }
   }
-}
-`
-
-
-
-
+`;
 
 export default function Header({ ...headerProps }) {
   return (
     <NavCont>
-      
+      <Hamburger>
+        <Bars3Icon />
+      </Hamburger>
+      <MenuCont>
+        <Link href="/">
+          <MyImg
+            src="/LOGO.svg"
+            alt="logo placeholder"
+            width="100px"
+            height="33px"
+          />
+        </Link>
+        <Link href="/about">
+          <Menu>About</Menu>
+        </Link>
+        <Link href="/learn">
+          <Menu>Learn</Menu>
+        </Link>
+      </MenuCont>
+      <MenuCont>
+        <Link href="/dashboard">
+          <Menu>Dashboard</Menu>
+        </Link>
+        <Link href="/account">
+          <Menu>My Account</Menu>
+        </Link>
         <Hamburger>
+          {/* This is just a placeholder for login button */}
           <Bars3Icon />
         </Hamburger>
-        <MenuCont>
-
-          <Link href="/">
-            
-              <MyImg
-                src="/LOGO.svg"
-                alt="logo placeholder"
-                width= '100px'
-                height = '33px'
-              />
-            
-          </Link>
-          <Link href="/about">
-            <Menu>About</Menu>
-          </Link>
-          <Link href="/learn">
-            <Menu>Learn</Menu>
-          </Link>
-        </MenuCont>
-        <MenuCont>
-          <Link href="/dashboard">
-            <Menu>Dashboard</Menu>
-          </Link>
-          <Link href="/account">
-            <Menu>My Account</Menu>
-          </Link>
-          <Hamburger>
-            {/* This is just a placeholder for login button */}
-            <Bars3Icon />
-          </Hamburger>
-        </MenuCont>
+      </MenuCont>
     </NavCont>
   );
 }
--- pages/account.jsx
+++ pages/account.jsx
@@ -1,4 +1,4 @@
-import Search from '../components/phishForm/PhishForm';
+import PhishForm from '../components/phishForm/PhishForm';
 import Header from '../components/header/Header';
 import styles from '../styles/Home.module.css';
 import Button from '../components/button/Button';
@@ -6,23 +6,26 @@ import { Container } from '../styles/global';
 // import {getUsers, setUsers} from "../firebase/users"
 import axios from 'axios';
 
-
-
 export default function Home() {
   const handleSendEmail = async (emailProperties) => {
-    // eslint-disable-next-line no-unused-vars
-    const res = await axios.post('/api/emailSent', {
+    await axios.post('/api/emailSent', {
       ...emailProperties,
       replyTo: 'phishedapp@gmail.com',
     });
   };
 
+  const handleScheduleEmail = (props) => {
+    axios.post('/api/emailScheduled', props);
+  };
 
   return (
     <>
       <Header></Header>
       <Container>
-        <Search onSendEmail={handleSendEmail} />
+        <PhishForm
+          onSendEmail={handleSendEmail}
+          onScheduleEmail={handleScheduleEmail}
+        />
       </Container>
     </>
   );
