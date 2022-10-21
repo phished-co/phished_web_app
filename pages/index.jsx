dd4281848dbf69cb2c5b8f2889aa1355aad3eca3
Trevor Lee <tlee297@my.bcit.ca>
2022-10-20 18:56:03 -0700

    create featurescards component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 // mantine imports
 import { HeroHeader } from "../components/mantine/heroHeader/HeroHeader"
+import { FeaturesCards } from "../components/mantine/features/Features"
 
 export default function Home() {
   return (
     <>
       <main>
         <HeroHeader />
+        <FeaturesCards />
       </main>
     </>
   )
