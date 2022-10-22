1cf722f6362b423b0d090627bfb21444bf0a1f34
SueLee <slee1008@my.bcit.ca>
2022-10-22 13:48:56 -0700

    Create email Input story

--- components/input/Input.jsx
+++ components/input/Input.jsx
@@ -1,22 +1,19 @@
-import { TextInput } from '@mantine/core';
+import { TextInput, InputBase } from '@mantine/core';
 
-export default function MyInput() {
+export default function MyInput({
+  label,
+  description,
+  placeholder,
+  inputWrapperOrder
+}) {
   return (
     <>
       <TextInput
-        label="Email Address"
-        description="Please enter your email"
-        placeholder="example@email.com"
-        inputWrapperOrder={['label', 'description', 'error', 'input']}
+        label={label}
+        description={description}
+        placeholder={placeholder}
+        inputWrapperOrder={inputWrapperOrder}
+        required
       />
-      {/* <TextInput
-        mt="xl"
-        label="Custom layout"
-        placeholder="Custom layout"
-        description="Error and description are"
-        error="both below the input"
-        inputWrapperOrder={['label', 'input', 'description', 'error']}
-      /> */}
     </>
-  );
-}
+    )}
