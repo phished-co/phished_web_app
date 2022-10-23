8bbb171f13c496374fe840a0bd3eed4da5a7df47
Trevor Lee <tlee297@my.bcit.ca>
2022-10-23 01:01:51 -0700

    about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,9 +1,31 @@
+import { Divider, Text, Title } from "@mantine/core";
+import styled from "styled-components";
+
+const Container = styled.div`
+  max-width: 1140px;
+  margin: 0 auto;
+  margin-top: 8rem;
+`
+
+const AboutContainer = styled.div`
+  text-align: center;
+  max-width: 60ch;
+  margin: 0 auto;
+`
+
 export default function About() {
   return (
-    <section>
-      <p>
-        this is where the components that say what are site is about should go
-      </p>
-    </section>
+    <Container>
+      <AboutContainer>
+        <Title mb={12}>About Phished</Title>
+        <Text>Phished is a non-profit organization that proudly empowers our users to educate their family members, friends, and neightbours on internet safety. Phished wil always be free and open-source. </Text>
+        <br />
+        <Text>Phished was developed for people to gain experience in dealing with phishing emails without exposing themselves to online fraud. Our phishing simulator provides hands-on training and is based on real-world examples of common phishing techniques. </Text>
+        <br />
+        <Text>If you have any questions, or want to get in contact, please send us an email at support@phished.app</Text>
+        <Divider mt={48} mb={48} />
+        <Text>Developed in Canada 🇨🇦 by Yasmina Amirifar, Sue Lee, Trevor Lee, Ivan Li, Lucia Martin, Sejin Oh, and Ivan Tong.</Text>
+      </AboutContainer>
+    </Container>
   );
 }
