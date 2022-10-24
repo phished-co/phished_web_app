e207eb7c40f4a1fd564fc50ed7481bbe7bc2515c
Trevor Lee <tlee297@my.bcit.ca>
2022-10-24 15:04:05 -0700

    max width

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -2,6 +2,8 @@ import ContactForm from "../components/mantine/contact/ContactForm";
 import styled from "styled-components";
 
 const Container = styled.div`
+  max-width: 768px;
+  margin: 0 auto;
   padding: 4rem 2rem;
 `
 
