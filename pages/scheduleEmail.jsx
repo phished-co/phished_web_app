bf726b100a8710b2c8d69371e6b4f7987b0339f5
Sejin Oh <soh52@my.bcit.ca>
2022-10-24 14:57:29 -0700

    store scheduledEmail in db and display it on a page

--- pages/scheduleEmail.jsx
+++ pages/scheduleEmail.jsx
@@ -1,7 +1,61 @@
+import { db } from '../firebaseConfig';
+import { useState, useEffect } from 'react';
+import { collection, doc, setDoc, getDocs } from '@firebase/firestore';
+
 export default function About() {
+  const [scheduleEmails, setScheduleEmails] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const scheduledEmailList = collection(db, 'scheduledEmails');
+
+  const getScheduledEmail = async () => {
+    const querySnapshot = await getDocs(scheduledEmailList);
+    let allDocs = [];
+    querySnapshot.forEach((doc) => {
+      allDocs.push(doc);
+    });
+    setScheduleEmails(allDocs);
+  };
+  
+  useEffect(() => {
+    // get the todos
+    getScheduledEmail();
+    // reset loading
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <section>
-      <p>this is for the user to schedule their email</p>
-    </section>
+    <div>
+      <h1>Scheduled Email List</h1>
+      {loading ? ( 
+          <h2> Loading... </h2>
+        ) : 
+          scheduleEmails.length === 0 ? (
+            <div>
+             <h2>No scheduled Email</h2>
+             <p>Consider adding a scheduled Email from <a href="/account">Click Here</a></p>
+            </div>
+           ) : (  
+            <div>
+              {scheduleEmails.map((email) => (
+                // console.log(email.id)
+                // console.log(email.data().postData)
+                <div key={email.id}>
+                  <p>Firstname: {email.data().postData.firstName}</p>
+                  <p>Lastname: {email.data().postData.lastName}</p>
+                  <p>SenderEmail: {email.data().postData.senderEmail}</p>
+                  <p>Receiver: {email.data().postData.receiver}</p>
+                  <p>Subject: {email.data().postData.subject}</p>
+                  <p>Message: {email.data().postData.message}</p>
+                  <br />
+                </div>
+              ))}
+            </div>
+          )
+        }
+    </div>
   );
 }
+
+
