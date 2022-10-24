4ef97fcd8b4bb30c6757422e82b6a97a985670a7
Yasmina Amir <66489192+Yasminaa77@users.noreply.github.com>
2022-10-23 21:37:03 -0700

    temporarily hold

--- pages/api/emailSent.js
+++ pages/api/emailSent.js
@@ -1,8 +1,9 @@
-
-import validate from 'deep-email-validator'
+// import emailSMTPVerificator from 'email-smtp-verificator';
+// import validate from 'deep-email-validator';
+// // import validate from 'email-validator';
 import { transporter } from '../../nodemailer';
 //emailSent.js is if the user decides to send their email right away (ie. without scheduling)
-
+import { verifyEmail } from '@devmehq/email-validator-js';
 
 export const config = {
   api:{ externalResolver: true, },
@@ -14,19 +15,27 @@ export default async function handler(req, res) {
     if (req.method === 'POST') {
     
       let senderInfo = req.body.from.split(' ')
-      let validationInfo = await validate(senderInfo[2])
-      console.log(validationInfo)
 
-      console.log(validationInfo)
+      //verifier
+      // let validationInfo =  await validate({email: senderInfo[2], verbose: true, timeout: 2000})
+      // console.log(validationInfo)
 
-
-      res.send(validationInfo.validators.mx)
-      if (!validationInfo.validators.mx) return
-
+      //deep validator
+      // let validationInfo = await validate(senderInfo[2])
+      // console.log(validationInfo)
       // res.send(validationInfo.valid)
       // if (!validationInfo.valid) return
 
-    
+
+      //verificator
+      // const verify = emailSMTPVerificator({ timeout: 12000 });
+      // console.log(await verify(senderInfo[2]))
+
+      //temporarly
+      res.send(true)
+
+
+
       transporter.sendMail(req.body, (error, info) => {
         if (error) console.log(error);
         else console.log('Email sent: ' + info.response);
