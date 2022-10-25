ce9a95a99b16a26a63fdfc0c13e49a981c36042c
SueLee <slee1008@my.bcit.ca>
2022-10-24 19:13:39 -0700

    sue - more variations on MyButton stories

--- components/button/Button.stories.jsx
+++ components/button/Button.stories.jsx
@@ -1,23 +1,39 @@
-import Button from './Button';
+import MyButton from './Button';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  title: 'Button',
-  component: Button,
+  title: 'MyButton',
+  component: MyButton,
   argTypes: {},
 };
 
-const Template = (args) => <Button {...args} />;
+const Template = (args) => <MyButton {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {
-  type: 'primary',
-  label: 'Button'
+  color: "blue",
+  variant: "fill",
+  size: "md",
+  txt: "Button",
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  ...Primary.args,
-  type: 'secondary',
-  label: 'Secondary'
+export const Disabled = Template.bind({});
+Disabled.args = {
+  txt: "Button",
+  disabled: true,
+};
+
+export const Outlined = Template.bind({});
+Outlined.args = {
+  color: "blue",
+  variant: "outline",
+  size: "md",
+  txt: "Outlined",
+};
+
+export const Lighter = Template.bind({});
+Lighter.args = {
+  variant: "light",
+  size: "md",
+  txt: "Lighter",
 };
