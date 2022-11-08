74d31ba618416aa356c468370c8def533d27ec6b
Lucia <luciammperu@gmail.com>
2022-11-08 11:49:50 -0800

    changed export

--- pages/api/emailSent.js
+++ pages/api/emailSent.js
@@ -4,78 +4,54 @@
 // import { verifyEmail } from '@devmehq/email-validator-js';
 import { transporter } from '../../nodemailer';
 //emailSent.js is if the user decides to send their email right away (ie. without scheduling)
-import hbs  from 'nodemailer-express-handlebars';
+import hbs from 'nodemailer-express-handlebars';
 import path from 'path';
 //attach the plugin to the nodemailer transporter
 // transporter.use('compile', hbs(options));
 // //send mail with options
 
-
-
 export const config = {
-    api: { externalResolver: true },
+  api: { externalResolver: true },
 };
 
 export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    res.send(true);
 
+    const handlebarOptions = {
+      viewEngine: {
+        extName: '.handlebars',
+        partialsDir: path.resolve('./templates'),
+        defaultLayout: false,
+      },
+      viewPath: path.resolve('./templates'),
+      extName: '.handlebars',
+    };
 
-    if (req.method === 'POST') {
-        res.send(true);
+    transporter.use('compile', hbs(handlebarOptions));
 
-        const handlebarOptions = {
-            viewEngine: {
-                extName: ".handlebars",
-                partialsDir: path.resolve('./templates'),
-                defaultLayout: false,
-            },
-            viewPath: path.resolve('./templates'),
-            extName:".handlebars",
-        }
+    var mailOptions = {
+      from: req.body.from,
+      to: req.body.to,
+      subject: req.body.subject,
+      replyTo: req.body.replyTo,
+      template: 'email',
+      context: {
+        text: req.body.html,
+      },
+    };
 
-
-        transporter.use('compile',hbs(handlebarOptions));
-
-
-        var mailOptions = {
-            from: req.body.from,
-            to: req.body.to,
-            subject: req.body.subject,
-            replyTo: req.body.replyTo,
-            template: 'email',
-            context: {
-                text: req.body.html
-            }
-        }
-
-
-        return new Promise((resolve, reject) => {
-            transporter
-                .sendMail(mailOptions)
-                .then((info) => {
-                    console.log(req.body)
-                    console.log('Email sent: ' + info.response);
-                    resolve(info);
-                })
-                .catch((e) => {
-                    reject(e.response);
-                });
+    return new Promise((resolve, reject) => {
+      transporter
+        .sendMail(mailOptions)
+        .then((info) => {
+          console.log(req.body);
+          console.log('Email sent: ' + info.response);
+          resolve(info);
+        })
+        .catch((e) => {
+          reject(e.response);
         });
-
-
-        //No Template Code
-        // return new Promise((resolve, reject) => {
-        //     transporter
-        //         .sendMail(req.body)
-        //         .then((info) => {
-        //             console.log(req.body)
-        //             console.log('Email sent: ' + info.response);
-        //             resolve(info);
-        //         })
-        //         .catch((e) => {
-        //             reject(e.response);
-        //         });
-        // });
-
-
-    }
+    });
+  }
 }
