08b1c0da51965778f39412cb562dbdc908bb6420
Lucia <luciammperu@gmail.com>
2022-11-08 02:00:44 -0800

    scheduling works but goes to promotions

--- pages/api/emailScheduled.js
+++ pages/api/emailScheduled.js
@@ -1,19 +1,25 @@
-import { getAllDocs, postDoc, getScheduledEmail, deleteScheduledEmail, updateScheduledEmail } from '../../firebase';
+import {
+  getAllDocs,
+  postDoc,
+  getScheduledEmail,
+  deleteScheduledEmail,
+  updateScheduledEmail,
+} from '../../firebase';
 
 export default async function handler(req, res) {
   //   if (req.method === 'POST') {
   console.log('we want to store this info for later -');
   console.log(req.body);
-  
+
   try {
     let emailInfo = {
       firstName: req.body.fname,
-      lastName : req.body.lname,
-      senderEmail : req.body.fromEmail,
-      receiver : req.body.to,
-      subject : req.body.subject,
-      message : req.body.html,
-    }
+      lastName: req.body.lname,
+      senderEmail: req.body.fromEmail,
+      receiver: req.body.to,
+      subject: req.body.subject,
+      message: req.body.html,
+    };
 
     if (req.method === 'POST') {
       const scheduledEmail = await postDoc('scheduledEmails', emailInfo);
