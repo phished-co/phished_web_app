e21495e2bd9505f1dab4a4be988f7253297f7913
Lucia-martin <90855217+Lucia-martin@users.noreply.github.com>
2022-11-16 00:49:44 -0800

    Revert "added auth w/ experimental firebase adapter version"

--- components/mantine/modal/Modal.jsx
+++ components/mantine/modal/Modal.jsx
@@ -1,44 +1,24 @@
 import { useState } from 'react';
 import { Modal, Button, Group } from '@mantine/core';
-import { AuthenticationForm } from '../authentication/Authentication';
-import { useRouter } from 'next/router';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { AuthenticationForm } from "../authentication/Authentication"
 
 export function MantineModal() {
-  const { data: session } = useSession();
-  console.log(session);
-  const r = useRouter();
-
   const [opened, setOpened] = useState(false);
 
   return (
     <>
-      <Modal opened={opened} onClose={() => setOpened(false)}>
+      <Modal
+        opened={opened}
+        onClose={() => setOpened(false)}
+      >
         <AuthenticationForm />
       </Modal>
 
       <Group position="center">
-        {/* <Button type='null' onClick={() => setOpened(true)}>Log in</Button> */}
-        {!session ? (
-          <Button type="null" onClick={signIn}>
-            Log in
-          </Button>
-        ) : (
-          // <Button
-          //   type="null"
-          //   onClick={() => {
-          //     r.push({ pathname: '/account' });
-          //   }}
-          // >
-          //   Account
-          // </Button>
-          <Button type="null" onClick={signOut}>
-            Sign out
-          </Button>
-        )}
+        <Button type='null' onClick={() => setOpened(true)}>Log in</Button>
       </Group>
     </>
   );
 }
 
-export default MantineModal;
+export default MantineModal
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,70 +1,51 @@
-import '../styles/globals.css';
+import '../styles/globals.css'
 import Head from 'next/head';
-import { useState } from 'react';
+import { useState } from "react";
 import '@tremor/react/dist/esm/tremor.css';
 import { ColorSchemeProvider, MantineProvider } from '@mantine/core';
 
 // Our imports
-import { FooterSimple } from '../components/mantine/footer/Footer';
-import { HeaderResponsive } from '../components/mantine/header/Header.tsx';
-import { links } from '../data/fake_data';
+import { FooterSimple } from "../components/mantine/footer/Footer"
+import { HeaderResponsive } from "../components/mantine/header/Header.tsx"
+import { links } from "../data/fake_data"
 
-import { SessionProvider } from 'next-auth/react';
-
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
-  // const { Component, pageProps } = props;
-  const [colorScheme, setColorScheme] = useState('light');
+export default function App(props) {
+  const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState('light')
 
   function toggleColorScheme(value) {
-    const nextColorScheme =
-      value || (colorScheme === 'dark' ? 'light' : 'dark');
+    const nextColorScheme = value || (colorScheme === 'dark' ? 'light' : 'dark');
     setColorScheme(nextColorScheme);
   }
 
   return (
     <>
-      <SessionProvider session={session}>
-        <ColorSchemeProvider
-          colorScheme={colorScheme}
-          toggleColorScheme={toggleColorScheme}
-        >
-          <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={{
-              colors: {
-                blue: [
-                  '#E8FBFF',
-                  '#B5E6FF',
-                  '#9ED8FF',
-                  '#88C9FE',
-                  '#71BAFE',
-                  '#5AABFE',
-                  '#449CFE',
-                  '#2D8EFD',
-                  '#177FFD',
-                  '#0070FD',
-                ],
-              },
-              colorScheme,
-            }}
-          >
-            <Head>
-              <title>Phished | Protect Friends & Family by Phishing Them</title>
-              <meta
-                name="viewport"
-                content="minimum-scale=1, initial-scale=1, width=device-width"
-              />
-            </Head>
-            <HeaderResponsive links={links} />
-            <Component {...pageProps} />
-            <FooterSimple />
-          </MantineProvider>
-        </ColorSchemeProvider>
-      </SessionProvider>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+        <MantineProvider withGlobalStyles withNormalizeCSS theme={{
+          colors: {
+            'blue': [
+              "#E8FBFF",
+              "#B5E6FF",
+              "#9ED8FF",
+              "#88C9FE",
+              "#71BAFE",
+              "#5AABFE",
+              "#449CFE",
+              "#2D8EFD",
+              "#177FFD",
+              "#0070FD"
+            ]
+          }, colorScheme
+        }}>
+          <Head>
+            <title>Phished | Protect Friends & Family by Phishing Them</title>
+            <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+          </Head>
+          <HeaderResponsive links={links} />
+          <Component {...pageProps} />
+          <FooterSimple />
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 }
