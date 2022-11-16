4ee238dfc1347d06b366a21aac97474b5bc92503
Yasmina Amir <66489192+Yasminaa77@users.noreply.github.com>
2022-11-15 19:09:25 -0800

    commenting scheduling for fb template

--- components/facebookForm/FacebookForm.jsx
+++ components/facebookForm/FacebookForm.jsx
@@ -203,15 +203,15 @@ export function FacebookForm({ submitHandler, onScheduleEmail }) {
               Send email
             </Button>
 
-            <Link href="/scheduleEmail" passHref>
-              <a
-                  onClick={() =>
-                      onScheduleEmail({ fname, lname, fromEmail, to, subject, html })
-                  }
-              >
-                <Button variant="subtle">Schedule email for later</Button>
-              </a>
-            </Link>
+            {/*<Link href="/scheduleEmail" passHref>*/}
+            {/*  <a*/}
+            {/*      onClick={() =>*/}
+            {/*          onScheduleEmail({ fname, lname, fromEmail, to, subject, html })*/}
+            {/*      }*/}
+            {/*  >*/}
+            {/*    <Button variant="subtle">Schedule email for later</Button>*/}
+            {/*  </a>*/}
+            {/*</Link>*/}
           </div>
         </form>
 
