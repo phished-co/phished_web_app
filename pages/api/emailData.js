5a8feea43be1c17122f3b34251cc6d3bdab2c183
Yasmina Amir <66489192+Yasminaa77@users.noreply.github.com>
2022-11-21 09:46:04 -0800

    last 6 months works

--- pages/api/emailData.js
+++ pages/api/emailData.js
@@ -27,7 +27,6 @@ export default async function handler(req, res) {
 
     //if the recent phished email id was not in phishedEmails collection add.
     if (!docSnap.exists()) {
-      // console.log("jabada")
       await setDoc(doc(db, "phishedEmails", phishedEmailId), sentEmailDocSnap.data());
     }
 
@@ -40,6 +39,28 @@ export default async function handler(req, res) {
     let userSentEmails = []
     let userPhishedEmails = []
 
+    let chartMonths = [];
+    let graphData = []
+
+    let monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+    let newMonth = 0;
+    var d = new Date();
+    for (let i = 0; i < 6; ++i) {
+
+      if (i === 0 ) {
+        newMonth = d.getMonth();
+      } else {
+        newMonth = d.getMonth() - 1;
+      }
+      d.setMonth(newMonth);
+      chartMonths.push({
+        date : `${monthNames[d.getMonth()]} ${d.getFullYear()}` ,
+        sent : 0,
+        success : 0
+      });
+
+    }
     //Get current month and past months function
     let currentDate = new Date()
     let currentMonth = currentDate.getMonth()
@@ -48,7 +69,7 @@ export default async function handler(req, res) {
       const formattedDate = new Date(date);
 
       date.setMonth(formattedDate.getMonth() - months);
-      return date.getMonth();
+      return date;
     }
 
 
@@ -57,7 +78,12 @@ export default async function handler(req, res) {
     const userSentEmailsQuery = query(collection(db, "sentEmails"), where("creatorEmail", "==", req.query.userEmail));
     const sentEmailsQuerySnapshot = await getDocs(userSentEmailsQuery);
     sentEmailsQuerySnapshot.forEach((doc) => {
-      userSentEmails.push(doc.data())
+      let newDate = new Date(doc.data().createdAt);
+      chartMonths.forEach((i) => {
+        if (`${monthNames[newDate.getMonth()]} ${newDate.getFullYear()}` === i.date) {
+          i.sent += 1;
+        }
+      });
     });
 
 
@@ -66,105 +92,15 @@ export default async function handler(req, res) {
     const userPhishedEmailsQuery = query(collection(db, "phishedEmails"), where("creatorEmail", "==", req.query.userEmail));
     const phishedEmailsQuerySnapshot = await getDocs(userPhishedEmailsQuery);
     phishedEmailsQuerySnapshot.forEach((doc) =>{
-      userPhishedEmails.push(doc.data());
+      let newDate = new Date(doc.data().createdAt);
+      chartMonths.forEach((i) => {
+        if (`${monthNames[newDate.getMonth()]} ${newDate.getFullYear()}` === i.date) {
+          i.success += 1;
+        }
+      })
     });
-
-    //month names
-    let monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-
-
-
-    //Emails quantity by month
-
-    let currentMonthSent = 0
-    let oneMonthAgoSent = 0
-    let twoMonthsAgoSent = 0
-    let threeMonthsAgoSent = 0
-    let fourMonthsAgoSent = 0
-    let fiveMonthsAgoSent = 0
-
-    let currentMonthPhished = 0
-    let oneMonthAgoPhished = 0
-    let twoMonthsAgoPhished = 0
-    let threeMonthsAgoPhished = 0
-    let fourMonthsAgoPhished = 0
-    let fiveMonthsAgoPhished = 0
-
-
-    // //    -----------------------------------------------------------------------------------------
-
-
-
-    let sentEmailsByMonth = userSentEmails.map(sentEmail=>{
-      let date = new Date(sentEmail.createdAt)
-      date.getMonth() == currentMonth && currentMonthSent++
-      date.getMonth() == subtractMonths(currentDate, 1) && oneMonthAgoSent++
-      date.getMonth() == subtractMonths(currentDate, 2) && twoMonthsAgoSent++
-      date.getMonth() == subtractMonths(currentDate, 3) && threeMonthsAgoSent++
-      date.getMonth() == subtractMonths(currentDate, 4) && fourMonthsAgoSent++
-      date.getMonth() == subtractMonths(currentDate, 5) && fiveMonthsAgoSent++
-
-
-    })
-
-    let phishedEmailsByMonth = userPhishedEmails.map(phishedEmail=>{
-      let date = new Date(phishedEmail.createdAt)
-      date.getMonth() == currentMonth && currentMonthPhished++
-      date.getMonth() == subtractMonths(currentDate, 1) && oneMonthAgoPhished++
-      date.getMonth() == subtractMonths(currentDate, 2) && twoMonthsAgoPhished++
-      date.getMonth() == subtractMonths(currentDate, 3) && threeMonthsAgoPhished++
-      date.getMonth() == subtractMonths(currentDate, 4) && fourMonthsAgoPhished++
-      date.getMonth() == subtractMonths(currentDate, 5) && fiveMonthsAgoPhished++
-
-
-    })
-
-
-    // let tweleveMonthsAgo = subtractMonths(currentDate, 12)
-    // console.log(`12 month ago was the month number ${tweleveMonthsAgo} in year  ???`)
-    console.log(`This month you had ${currentMonthPhished} success out of ${currentMonthSent}`)
-
-
-//    -----------------------------------------------------------------------------------------
-
-
-
-let graphData = [
-  {
-    date: "Jan 2022",
-    sent: 6,
-    success: 4,
-  },
-  {
-    date: "Feb 2022",
-    sent: 10,
-    success: 8,
-  },
-  {
-    date: "Mar 2022",
-    sent: 14,
-    success: 14,
-  },
-  {
-    date: "Apr 2022",
-    sent: 21,
-    success: 12,
-  },
-  {
-    date: "May 2022",
-    sent: 25,
-    success: 14,
-  },
-  {
-    date: "Jun 2022",
-    sent: 32,
-    success: 15,
-  },
-
-]
-
-
-    res.send([])
+    graphData = chartMonths;
+    console.log(graphData)
 
 
 
