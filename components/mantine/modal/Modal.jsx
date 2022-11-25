e9b0fe64b1f4c7eb085960ce8b76b3682ea90886
Lucia-martin <90855217+Lucia-martin@users.noreply.github.com>
2022-11-24 18:18:16 -0800

    Revert "testing fix for auth redirect"

--- components/mantine/modal/Modal.jsx
+++ components/mantine/modal/Modal.jsx
@@ -1,30 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Modal, Button, Group } from '@mantine/core';
 import { AuthenticationForm } from '../authentication/Authentication';
 import { useRouter } from 'next/router';
 import { signIn, signOut, useSession } from 'next-auth/react';
-import { getCsrfToken } from 'next-auth/react';
 
 export function MantineModal() {
-  const { data: session, status } = useSession();
-
-  console.log(useSession());
+  const { data: session } = useSession();
   const r = useRouter();
   const [opened, setOpened] = useState(false);
-  const [csrfToken, setCsrfToken] = useState('');
-  useEffect(() => {
-    async function fetchCsrfToken() {
-      const result = await getCsrfToken();
-      if (!result) {
-        throw new Error('Can not sign in without a CSRF token');
-      }
-      setCsrfToken(result);
-    }
 
-    if (status !== 'loading') {
-      fetchCsrfToken();
-    }
-  }, [status]);
   return (
     <>
       <Modal opened={opened} onClose={() => setOpened(false)}>
