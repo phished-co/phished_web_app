16bd1f50593873383efb3fa89ceab9f0c7363bf6
Trevor Lee <tlee297@my.bcit.ca>
2022-11-27 01:55:15 -0800

    functional buttons

--- components/quiz/EmailQuestion.jsx
+++ components/quiz/EmailQuestion.jsx
@@ -1,14 +1,19 @@
 import { Button } from "@mantine/core"
+import styled from "styled-components"
 
-export default function EmailQuestion({ questionIds }) {
+const Wrapper = styled.div`
+  outline: 2px solid red;
+`
+
+export default function EmailQuestion({ questionIds, questionNum }) {
   let list = questionIds.map(question => {
     return <h1 key={question.id}>{question.name}</h1>
   })
 
   return (
-    <>
-      <Button type='null' onClick={() => { console.log(questionIds) }}>Test</Button>
+    <Wrapper>
+      {questionNum}
       {list}
-    </>
+    </Wrapper>
   )
 }
--- pages/quiz.jsx
+++ pages/quiz.jsx
@@ -6,7 +6,16 @@ import EmailQuestion from "../components/quiz/EmailQuestion"
 export const Container = styled.div`
   max-width: 1140px;
   margin: 0 auto;
-  margin-top: 8vh
+  margin-top: 8vh;
+  outline: 2px solid green;
+
+  .btn-cont {
+    display: flex;
+    gap: 2rem;
+    outline: 2px solid orange;
+    max-width: 640px;
+    margin: 0 auto;
+  }
 `
 const questionBank = [
   {
@@ -41,6 +50,7 @@ export default function Quiz() {
   let questions = questionBank.slice(0)
   const [showScore, setShowScore] = useState(false)
   const [shuffledQuestions, setShuffledQuestions] = useState(questions)
+  const [questionNum, setQuestionNum] = useState(0)
 
 
   // Fisher-Yates shuffle method
@@ -55,17 +65,26 @@ export default function Quiz() {
     console.log(questions)
     setShowScore(false)
     setShuffledQuestions(questions)
+    setQuestionNum(0)
+  }
+
+  function handleClick() {
+    setQuestionNum(questionNum => questionNum + 1)
+
+    if (questionNum > 1) {
+      setShowScore(true)
+    }
   }
 
   return (
     <Container>
+      <EmailQuestion questionIds={shuffledQuestions.slice(0, 3)} questionNum={questionNum} />
       {showScore
         ? <Button type='null' onClick={shuffle}>Reset quiz</Button>
         : <div className='btn-cont'>
-          <Button type='null' onClick={() => { setShowScore(true) }}>Legitimate</Button>
-          <Button type='null'>Fradulent</Button>
+          <Button type='null' fullWidth onClick={handleClick}>Legitimate</Button>
+          <Button type='null' fullWidth onClick={handleClick}>Fradulent</Button>
         </div>}
-      <EmailQuestion questionIds={shuffledQuestions.slice(0, 5)} />
     </Container>
   )
 }
