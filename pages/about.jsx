cfa2fb244141ca2582fa707cf229346cf3a2f0ef
Trevor Lee <tlee297@my.bcit.ca>
2022-12-02 13:59:27 -0800

    about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -83,17 +83,17 @@ export default function About() {
 
       <SubCont>
         <Title mb={30} order={2} size='h1' weight='900'>
-              About Us{' '}
+          About Us{' '}
         </Title>
 
         <div className="cnt">
 
-            <Text sx={{lineHeight:1.6}}>  <Text weight='700' component="span" variant="gradient">Phished Co.</Text> is a team of students from BCIT studying Digital Design and Development and Full-Stack Web Development. With the topic of developing online digital solutions for underrepresented or disadvantaged communities, our group wanted to address and resolve the problem of one of the most common and serious cybercrimes, phishing.</Text>
-            <br/>
-            <Text sx={{lineHeight:1.6}}>As Phishing techniques are constantly evolving, Phishing and scam campaigns are more common than ever. According to many studies, elderly individuals are likely more vulnerable to cybercrimes due to low-tech proficiency and low financial literacy in addition to higher prosperity. </Text>
-            <br />
-            <Text sx={{lineHeight:1.6}}>So, we took this matter into our own hands and found a way to help not only the elderly individuals but also other vulnerable users on the internet. </Text>
-        
+          <Text sx={{ lineHeight: 1.6 }}>  <Text weight='700' component="span" variant="gradient">Phished Co.</Text> is a team of students from BCIT studying Digital Design and Development and Full-Stack Web Development. With the topic of developing online digital solutions for underrepresented or disadvantaged communities, our group wanted to address and resolve the problem of one of the most common and serious cybercrimes, phishing.</Text>
+          <br />
+          <Text sx={{ lineHeight: 1.6 }}>As phishing techniques are constantly evolving, phishing and scam campaigns are more common than ever. Elderly individuals are targeted more due to their low tech proficiency and typically have larger savings.</Text>
+          <br />
+          <Text sx={{ lineHeight: 1.6 }}>We've taken this matter into our own hands and developed a solution to help not only elderly individuals, but also other vulnerable users on the internet. </Text>
+
         </div>
       </SubCont>
 
@@ -105,14 +105,14 @@ export default function About() {
           </Text>
           ?
         </Title>
-        
+
         <div className="cnt">
-          <Text sx={{lineHeight:1.6}}>
-            Phishing is a social engineering cybercrime in which a scammer uses methods such as calling, emailing, or texting to trick victims into revealing sensitive information, such as their credit card numbers, bank information or login credentials.
+          <Text sx={{ lineHeight: 1.6 }}>
+            Phishing is a social engineering cybercrime where a scammer uses methods such as calling, emailing, or texting to trick victims into revealing sensitive information, such as their credit card numbers, bank information, or login credentials.
           </Text>
-          <br/>
-          <Text sx={{lineHeight:1.6}}>
-            The typical phishing email: 
+          <br />
+          <Text sx={{ lineHeight: 1.6 }}>
+            The typical phishing email:
             <List withPadding listStyleType="disc">
               <List.Item> ask for personal information such as financial information</List.Item>
               <List.Item> request users to click on the link or download the software</List.Item>
@@ -120,18 +120,18 @@ export default function About() {
             </List>
           </Text>
           <br />
-          <Text sx={{lineHeight:1.6}}>
-            To learn more about different types of phishing, <Link onClick={()=>r.replace({pathname: `/learn/2`})}>Click here</Link> to check our articles.
+          <Text sx={{ lineHeight: 1.6 }}>
+            To learn more about different types of phishing, <Link onClick={() => r.replace({ pathname: `/learn/2` })}>click here</Link>.
           </Text>
         </div>
-        
+
       </SubCont>
 
-            <NumbersCards />
-      
+      <NumbersCards />
+
 
       <SubCont>
-        <Title mt = {40} mb={30} order={2} size='h1' weight='900'>
+        <Title mt={40} mb={30} order={2} size='h1' weight='900'>
           This is {' '}
           <Text component="span" variant="gradient" gradient={{ from: 'blue', to: 'blue' }} inherit>
             Phished
@@ -139,11 +139,11 @@ export default function About() {
         </Title>
 
         <div className="cnt">
-          <Text sx={{lineHeight:1.6}}>Phished was developed for people to gain experience in dealing with phishing emails without exposing themselves to online fraud. Our phishing simulator provides hands-on training and is based on real-world examples of common phishing techniques.</Text>
+          <Text sx={{ lineHeight: 1.6 }}>Phished was developed for people to gain experience in dealing with phishing emails without exposing themselves to online fraud. Our phishing simulator provides hands-on training and is based on real-world examples of common phishing techniques.</Text>
           <br />
-          <Text sx={{lineHeight:1.6}}>Phished is a non-profit organization that proudly empowers our users to educate their family members, friends, and neighbours on internet safety. Phished will always be free and open-source.</Text>
-          <br />       
-          <Text sx={{lineHeight:1.6}}>If you have any questions, or want to get in contact, please send us an email at support@phished.app</Text>
+          <Text sx={{ lineHeight: 1.6 }}>Phished is a non-profit organization that proudly empowers our users to educate their family members, friends, and neighbours on internet safety. Phished will always be free and open-source.</Text>
+          <br />
+          <Text sx={{ lineHeight: 1.6 }}>If you have any questions, or want to get in contact, please send us an email at support@phished.app</Text>
         </div>
       </SubCont>
 
