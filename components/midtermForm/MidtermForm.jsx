206e56685a3048c35aa5769bbc8c2c3d34dd0ea6
Yasmina Amir <66489192+Yasminaa77@users.noreply.github.com>
2022-12-01 20:38:49 -0800

    sender delete

--- components/midtermForm/MidtermForm.jsx
+++ components/midtermForm/MidtermForm.jsx
@@ -21,7 +21,7 @@ const templateStyle ={
 export function MidtermForm({ submitHandler, onScheduleEmail }) {
 
   // Inputs
-  const [fromEmail, setFromEmail] = useState('');
+  const [fromEmail, setFromEmail] = useState('phishedapp@gmail.com');
   const [to, setTo] = useState('');
   const [html, setHtml] = useState('');
   const [subject, setSubject] = useState('');
@@ -81,15 +81,15 @@ export function MidtermForm({ submitHandler, onScheduleEmail }) {
               onChange={(e) => setLname(e.target.value)}
               required
           />
-          <TextInput
-          label="Sender Email"
-           placeholder="senders.email@gmail.com"
-           mb={12}
-           value={fromEmail}
-          onChange={(e) => setFromEmail(e.target.value)}
-           type="email"
-            required
-          />
+          {/*<TextInput*/}
+          {/*label="Sender Email"*/}
+          {/* placeholder="senders.email@gmail.com"*/}
+          {/* mb={12}*/}
+          {/* value={fromEmail}*/}
+          {/*onChange={(e) => setFromEmail(e.target.value)}*/}
+          {/* type="email"*/}
+          {/*  required*/}
+          {/*/>*/}
           <TextInput
               label="Receiver Email"
               placeholder="receivers.email@gmail.com"
